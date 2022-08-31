ad71239bb0a13c6d2f1e91d43e5ff340d7e32b38
JoshuaDodds <jdodds@local>
2022-08-31 19:00:34 +0200

    Dont show custom solar string and battery data metrics if its not available.
    Signed-off-by: JoshuaDodds <jdodds@local>

--- src/app/MarineApp/components/Battery/Battery.tsx
+++ src/app/MarineApp/components/Battery/Battery.tsx
@@ -1,50 +1,37 @@
-import React, { Component, useMemo } from "react";
+import React, { Component, useMemo } from "react"
 
-import {
-  Battery,
-  useBattery,
-  useTopicsState,
-  useTopicSubscriptions,
-} from "@elninotech/mfd-modules";
-import { BATTERY_STATE } from "../../../utils/constants";
+import { Battery, useBattery, useTopicsState, useTopicSubscriptions } from "@elninotech/mfd-modules"
+import { BATTERY_STATE } from "../../../utils/constants"
 
-import ColumnContainer from "../ColumnContainer";
-import BatteryIcon from "../../images/icons/battery.svg";
-import classnames from "classnames";
-import { ListViewWithTotals, ListRow } from "../ListViewWithTotals";
-import NumericValue from "../../../components/NumericValue";
-import MetricValues from "../MetricValues";
-import SelectorButton from "../SelectorButton";
-import { BatteryLevel } from "./BatteryLevel";
+import ColumnContainer from "../ColumnContainer"
+import BatteryIcon from "../../images/icons/battery.svg"
+import classnames from "classnames"
+import { ListViewWithTotals, ListRow } from "../ListViewWithTotals"
+import NumericValue from "../../../components/NumericValue"
+import MetricValues from "../MetricValues"
+import SelectorButton from "../SelectorButton"
+import { BatteryLevel } from "./BatteryLevel"
 
-import "./Battery.scss";
+import "./Battery.scss"
 
-import LIcon from "../../images/icons/L.svg";
-import RIcon from "../../images/icons/R.svg";
-import { translate } from "react-i18nify";
-import { observer } from "mobx-react";
-import { useVisibilityNotifier } from "app/MarineApp/modules";
-import { WIDGET_TYPES } from "app/MarineApp/utils/constants";
+import LIcon from "../../images/icons/L.svg"
+import RIcon from "../../images/icons/R.svg"
+import { translate } from "react-i18nify"
+import { observer } from "mobx-react"
+import { useVisibilityNotifier } from "app/MarineApp/modules"
+import { WIDGET_TYPES } from "app/MarineApp/utils/constants"
 
 type PaginatorProps = {
-  setPage: Function;
-  currentPage: number;
-  pages: number;
-};
+  setPage: Function
+  currentPage: number
+  pages: number
+}
 
 const Paginator = ({ setPage, currentPage, pages }: PaginatorProps) => {
   return (
     <div className="battery__paginator">
-      <SelectorButton
-        alwaysUnlocked={true}
-        disabled={currentPage < 1}
-        onClick={() => setPage(currentPage - 1)}
-      >
-        <img
-          src={LIcon}
-          className="battery__paginator-button"
-          alt={"Pagination button left"}
-        />
+      <SelectorButton alwaysUnlocked={true} disabled={currentPage < 1} onClick={() => setPage(currentPage - 1)}>
+        <img src={LIcon} className="battery__paginator-button" alt={"Pagination button left"} />
       </SelectorButton>
       <span className="battery__paginator-page">{currentPage + 1}</span>
       <SelectorButton
@@ -52,117 +39,92 @@ const Paginator = ({ setPage, currentPage, pages }: PaginatorProps) => {
         disabled={currentPage + 1 >= pages}
         onClick={() => setPage(currentPage + 1)}
       >
-        <img
-          src={RIcon}
-          className="battery__paginator-button"
-          alt={"Pagination button right"}
-        />
+        <img src={RIcon} className="battery__paginator-button" alt={"Pagination button right"} />
       </SelectorButton>
     </div>
-  );
-};
+  )
+}
 
 type BatteryHeaderProps = {
-  amount: number;
-  paginate: boolean;
-  setPage: Function;
-  currentPage: number;
-  pageSize: number;
-};
+  amount: number
+  paginate: boolean
+  setPage: Function
+  currentPage: number
+  pageSize: number
+}
 
-const BatteryHeader = ({
-  amount,
-  paginate,
-  setPage,
-  currentPage,
-  pageSize,
-}: BatteryHeaderProps) => {
-  let primaryBatteryPower = useBattery().batteries[amount - amount].power;
+const BatteryHeader = ({ amount, paginate, setPage, currentPage, pageSize }: BatteryHeaderProps) => {
+  let primaryBatteryPower = useBattery().batteries[amount - amount].power
 
   return (
     <ListViewWithTotals
       icon={BatteryIcon}
       title={translate("widgets." + (amount > 1 ? "batteries" : "battery"))}
       totals={primaryBatteryPower}
-      subTitle={
-        amount +
-        " " +
-        translate("widgets." + (amount > 1 ? "batteries" : "battery"))
-      }
+      subTitle={amount + " " + translate("widgets." + (amount > 1 ? "batteries" : "battery"))}
       child={true}
     >
       <ListRow>
-        {paginate && (
-          <Paginator
-            setPage={setPage}
-            currentPage={currentPage}
-            pages={Math.ceil(amount / pageSize)}
-          />
-        )}
+        {paginate && <Paginator setPage={setPage} currentPage={currentPage} pages={Math.ceil(amount / pageSize)} />}
       </ListRow>
     </ListViewWithTotals>
-  );
-};
+  )
+}
 
 const BatteryRowMainInfo = (battery: Battery) => {
   return (
     <MetricValues inflate>
       <div className="metrics__left">
-        <NumericValue
-          value={battery.voltage}
-          unit="V"
-          defaultValue={null}
-          precision={1}
-        />
-        <NumericValue
-          value={battery.current}
-          unit="A"
-          defaultValue={null}
-          precision={1}
-        />
+        <NumericValue value={battery.voltage} unit="V" defaultValue={null} precision={1} />
+        <NumericValue value={battery.current} unit="A" defaultValue={null} precision={1} />
         <NumericValue value={battery.power} unit="W" defaultValue={null} />
       </div>
       {battery.soc !== undefined && <BatteryLevel battery={battery} />}
     </MetricValues>
-  );
-};
+  )
+}
 
 const BatteryRowAdditionalInfo = (battery: Battery) => {
-  const temperature_min_max =
-    useCustomBatteryMetrics().min_cell_temp +
-    "° / " +
-    useCustomBatteryMetrics().max_cell_temp +
-    "°";
+  const {
+    min_cell_temp,
+    max_cell_temp,
+    min_cell_volt,
+    max_cell_volt,
+    modules_online,
+    modules_offline,
+    capacity_available,
+    capacity_installed,
+    state_of_health,
+  } = useCustomBatteryMetrics()
+  const temperature_min_max = min_cell_temp + "° / " + max_cell_temp + "°"
   const cell_voltage_min_max =
-    parseFloat(useCustomBatteryMetrics().min_cell_volt).toFixed(2) +
-    "v min " +
-    parseFloat(useCustomBatteryMetrics().max_cell_volt).toFixed(2) +
-    "v max";
+    parseFloat(min_cell_volt).toFixed(2) + "v min " + parseFloat(max_cell_volt).toFixed(2) + "v max"
+
+  const additionalInfo = !!((min_cell_temp && min_cell_volt && capacity_available && modules_online) || false)
 
   return (
     <MetricValues inflate>
-      <div className="metrics__left text--subtitle-upper">
-        <ColumnContainer>
-          <span>
-            &nbsp;{temperature_min_max}
-            <br /> min / max
-          </span>
-        </ColumnContainer>
-        <ColumnContainer>
-          <span>{cell_voltage_min_max}</span>
-        </ColumnContainer>
-        <ColumnContainer>
-          {useCustomBatteryMetrics().capacity_available} /{" "}
-          {useCustomBatteryMetrics().capacity_installed} Ah <br />
-          SoH: {useCustomBatteryMetrics().state_of_health}%
-        </ColumnContainer>
-        <ColumnContainer>
-          {useCustomBatteryMetrics().modules_online} Modules Online
-        </ColumnContainer>
-      </div>
+      {additionalInfo && (
+        <div className="metrics__left text--subtitle-upper">
+          <ColumnContainer>
+            <span>
+              &nbsp;{temperature_min_max}
+              <br /> min / max
+            </span>
+          </ColumnContainer>
+          <ColumnContainer>
+            <span>{cell_voltage_min_max}</span>
+          </ColumnContainer>
+          <ColumnContainer>
+            {capacity_available} / {capacity_installed} Ah <br />
+            SoH: {state_of_health}%
+          </ColumnContainer>
+          <ColumnContainer>{modules_online} Modules Online</ColumnContainer>
+        </div>
+      )}
     </MetricValues>
-  );
-};
+  )
+}
 
 const SingleBattery = (battery: Battery) => (
   <ListViewWithTotals
@@ -179,28 +141,24 @@ const SingleBattery = (battery: Battery) => (
       <BatteryRowAdditionalInfo {...battery} />
     </ListRow>
   </ListViewWithTotals>
-);
+)
 
 type BatteryListProps = {
-  batteries: Array<Battery | any>;
-  currentPage: number;
-  pageSize: number;
-};
+  batteries: Array<Battery | any>
+  currentPage: number
+  pageSize: number
+}
 
 class BatteryList extends Component<BatteryListProps> {
-  ref = React.createRef<HTMLDivElement>();
+  ref = React.createRef<HTMLDivElement>()
 
   render() {
-    const { batteries } = this.props;
+    const { batteries } = this.props
     return (
       <div
         className="batteries"
         ref={this.ref}
-        style={
-          this.ref.current && batteries.some((b) => b.dummy)
-            ? { height: this.ref.current!.offsetHeight }
-            : {}
-        }
+        style={this.ref.current && batteries.some((b) => b.dummy) ? { height: this.ref.current!.offsetHeight } : {}}
       >
         {batteries.map((battery, i) => {
           return (
@@ -212,74 +170,68 @@ class BatteryList extends Component<BatteryListProps> {
             >
               {!battery.dummy && (
                 <div className="battery__data">
-                  <div className="battery__title-row text--subtitle-upper">
-                    {battery.name}
-                  </div>
+                  <div className="battery__title-row text--subtitle-upper">{battery.name}</div>
                   <BatteryRowMainInfo {...(battery as Battery)} />
                   <BatteryRowAdditionalInfo {...(battery as Battery)} />
                 </div>
               )}
             </div>
-          );
+          )
         })}
       </div>
-    );
+    )
   }
 }
 
 type BatteriesProps = {
-  batteries: Array<Battery>;
-};
+  batteries: Array<Battery>
+}
 type BatteriesState = {
-  pageSize: number;
-  currentPage: number;
-};
+  pageSize: number
+  currentPage: number
+}
 
 export class Batteries extends Component<BatteriesProps, BatteriesState> {
-  state = { pageSize: 1, currentPage: 0 };
-  ref = React.createRef();
+  state = { pageSize: 1, currentPage: 0 }
+  ref = React.createRef()
 
   setPage = (currentPage: number) => {
-    this.setState({ currentPage });
-  };
+    this.setState({ currentPage })
+  }
 
   updatePageSize() {
-    const pageSize =
-      window.innerHeight < 500 ? 1 : window.innerHeight < 700 ? 2 : 3;
+    const pageSize = window.innerHeight < 500 ? 1 : window.innerHeight < 700 ? 2 : 3
 
     if (pageSize !== this.state.pageSize) {
-      this.setState({ pageSize });
-      this.setPage(0);
+      this.setState({ pageSize })
+      this.setPage(0)
     }
   }
 
   componentDidMount() {
-    this.updatePageSize();
-    window.addEventListener("resize", this.updatePageSize.bind(this));
+    this.updatePageSize()
+    window.addEventListener("resize", this.updatePageSize.bind(this))
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.updatePageSize.bind(this));
+    window.removeEventListener("resize", this.updatePageSize.bind(this))
   }
 
   render() {
-    const { batteries } = this.props;
-    const pageSize = this.state.pageSize;
-    const paginate = batteries.length > pageSize;
-    const currentPage = this.state.currentPage;
+    const { batteries } = this.props
+    const pageSize = this.state.pageSize
+    const paginate = batteries.length > pageSize
+    const currentPage = this.state.currentPage
 
     const batteriesToShow: Array<any> = paginate
-      ? batteries.slice(
-          currentPage * pageSize,
-          currentPage * pageSize + pageSize
-        )
-      : batteries;
+      ? batteries.slice(currentPage * pageSize, currentPage * pageSize + pageSize)
+      : batteries
     // These fill the last page with empty elements if necessary
     const fillerBatteries = paginate
       ? [...Array(pageSize - batteriesToShow.length)].map(() => ({
           dummy: true,
         }))
-      : [];
+      : []
 
     const MultipleBatteries = (
       <div className="metric metric__battery">
@@ -296,72 +248,48 @@ export class Batteries extends Component<BatteriesProps, BatteriesState> {
           pageSize={pageSize}
         />
       </div>
-    );
+    )
 
-    return batteries.length === 1
-      ? SingleBattery(batteries[0])
-      : MultipleBatteries;
+    return batteries.length === 1 ? SingleBattery(batteries[0]) : MultipleBatteries
   }
 }
 
 export const BatteriesWithData = observer(() => {
-  const { batteries } = useBattery();
+  const { batteries } = useBattery()
 
   useVisibilityNotifier({
     widgetName: WIDGET_TYPES.BATTERY,
     visible: !!(batteries && batteries.length),
-  });
+  })
 
   if (batteries) {
     // Sort batteries first by state, and then by ID to keep order consistent
     const sorted = batteries.slice().sort((a, b) => {
       // Show charging batteries before discharging batteries
-      if (
-        a.state === BATTERY_STATE.CHARGING &&
-        b.state !== BATTERY_STATE.CHARGING
-      )
-        return -1;
-      if (
-        a.state !== BATTERY_STATE.CHARGING &&
-        b.state === BATTERY_STATE.CHARGING
-      )
-        return 1;
-      if (
-        a.state === BATTERY_STATE.CHARGING &&
-        b.state === BATTERY_STATE.CHARGING
-      ) {
-        return parseInt(a.id) - parseInt(b.id);
+      if (a.state === BATTERY_STATE.CHARGING && b.state !== BATTERY_STATE.CHARGING) return -1
+      if (a.state !== BATTERY_STATE.CHARGING && b.state === BATTERY_STATE.CHARGING) return 1
+      if (a.state === BATTERY_STATE.CHARGING && b.state === BATTERY_STATE.CHARGING) {
+        return parseInt(a.id) - parseInt(b.id)
       }
 
       // Show discharging batteries before idle batteries
-      if (
-        a.state === BATTERY_STATE.DISCHARGING &&
-        b.state === BATTERY_STATE.IDLE
-      )
-        return -1;
-      if (
-        a.state === BATTERY_STATE.IDLE &&
-        b.state === BATTERY_STATE.DISCHARGING
-      )
-        return 1;
-      if (
-        a.state === BATTERY_STATE.DISCHARGING &&
-        b.state === BATTERY_STATE.DISCHARGING
-      ) {
-        return parseInt(a.id) - parseInt(b.id);
+      if (a.state === BATTERY_STATE.DISCHARGING && b.state === BATTERY_STATE.IDLE) return -1
+      if (a.state === BATTERY_STATE.IDLE && b.state === BATTERY_STATE.DISCHARGING) return 1
+      if (a.state === BATTERY_STATE.DISCHARGING && b.state === BATTERY_STATE.DISCHARGING) {
+        return parseInt(a.id) - parseInt(b.id)
       }
 
-      return parseInt(a.id) - parseInt(b.id);
-    });
+      return parseInt(a.id) - parseInt(b.id)
+    })
     return (
       <ColumnContainer>
         <Batteries batteries={sorted} />
       </ColumnContainer>
-    );
+    )
   } else {
-    return <div />;
+    return <div />
   }
-});
+})
 
 function useCustomBatteryMetrics() {
   const getTopics = function () {
@@ -375,13 +303,13 @@ function useCustomBatteryMetrics() {
       capacity_available: "N/48e7da878d35/battery/512/Capacity",
       capacity_installed: "N/48e7da878d35/battery/512/InstalledCapacity",
       state_of_health: "N/48e7da878d35/battery/512/Soh",
-    };
-  };
+    }
+  }
   const topics = useMemo(function () {
-    return getTopics();
-  }, []);
-  useTopicSubscriptions(topics);
-  return useTopicsState(topics);
+    return getTopics()
+  }, [])
+  useTopicSubscriptions(topics)
+  return useTopicsState(topics)
 }
 
-export default BatteriesWithData;
+export default BatteriesWithData
--- src/app/MarineApp/components/Solar/Solar.tsx
+++ src/app/MarineApp/components/Solar/Solar.tsx
@@ -1,27 +1,38 @@
-import React, { useMemo } from "react";
+import React, { useMemo } from "react"
 
-import ColumnContainer from "../ColumnContainer";
-import MetricValues from "../MetricValues";
-import NumericValue from "../../../components/NumericValue";
+import ColumnContainer from "../ColumnContainer"
+import MetricValues from "../MetricValues"
+import NumericValue from "../../../components/NumericValue"
 
-import SolarIcon from "../../images/icons/icon_solar.svg";
-import {
-  usePvCharger,
-  useTopicsState,
-  useTopicSubscriptions,
-} from "@elninotech/mfd-modules";
-import { observer } from "mobx-react";
-import { useVisibilityNotifier } from "app/MarineApp/modules";
-import { WIDGET_TYPES } from "app/MarineApp/utils/constants";
-import { translate } from "react-i18nify";
-import { ListViewWithTotals } from "../ListViewWithTotals";
+import SolarIcon from "../../images/icons/icon_solar.svg"
+import { usePvCharger, useTopicsState, useTopicSubscriptions } from "@elninotech/mfd-modules"
+import { observer } from "mobx-react"
+import { useVisibilityNotifier } from "app/MarineApp/modules"
+import { WIDGET_TYPES } from "app/MarineApp/utils/constants"
+import { translate } from "react-i18nify"
+import { ListViewWithTotals } from "../ListViewWithTotals"
 
 const Solar = observer(() => {
-  const { current, power } = usePvCharger();
-  const pvExtra = usePvDetail();
-  const visible = !!(current || power || power === 0);
-  const daily_yield = pvExtra.c1_daily_yield + pvExtra.c2_daily_yield;
-  useVisibilityNotifier({ widgetName: WIDGET_TYPES.SOLAR, visible });
+  const { current, power } = usePvCharger()
+  const {
+    string_a_volts,
+    string_b_volts,
+    string_c_volts,
+    string_d_volts,
+    c1_daily_yield,
+    c2_daily_yield,
+    string_a_power,
+    string_b_power,
+    string_c_power,
+    string_d_power,
+  } = usePvDetail()
+
+  const visible = !!(current || power || power === 0)
+  const extraVisible = !!((string_a_volts && string_b_volts && string_c_volts && string_d_volts) || false)
+
+  const daily_yield = c1_daily_yield + c2_daily_yield
+
+  useVisibilityNotifier({ widgetName: WIDGET_TYPES.SOLAR, visible })
 
   if (visible) {
     return (
@@ -34,100 +45,54 @@ const Solar = observer(() => {
           child={false}
         >
           <MetricValues>
-            <span className="text--small text--subtitle-upper">
-              Production Today&nbsp;
-            </span>
-            <NumericValue value={daily_yield} unit="kWh" precision={2} />
-            <span className="text--small text--subtitle-upper">
-              Pv Current&nbsp;
-            </span>
+            {extraVisible && (
+              <span>
+                <span className="text--small text--subtitle-upper">Production Today&nbsp;</span>
+                <NumericValue value={daily_yield} unit="kWh" precision={2} />
+              </span>
+            )}
+            <span className="text--small text--subtitle-upper">Pv Current&nbsp;</span>
             <NumericValue value={current} unit=" Amps" precision={1} />
           </MetricValues>
-          <MetricValues inflate>
-            <div className="text--smaller">
-              <table cellPadding="0" cellSpacing="5">
-                <tr>
-                  <td>
-                    <span className="text--small text--subtitle-upper">
-                      String A&nbsp;
-                    </span>
-                    <NumericValue
-                      value={pvExtra.string_a_volts}
-                      unit="V"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                    <NumericValue
-                      value={pvExtra.string_a_power}
-                      unit="W"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                  </td>
-                  <td>
-                    <span className="text--small text--subtitle-upper">
-                      String B&nbsp;
-                    </span>
-                    <NumericValue
-                      value={pvExtra.string_b_volts}
-                      unit="V"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                    <NumericValue
-                      value={pvExtra.string_b_power}
-                      unit="W"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="text--small text--subtitle-upper">
-                      String C&nbsp;
-                    </span>
-                    <NumericValue
-                      value={pvExtra.string_c_volts}
-                      unit="V"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                    <NumericValue
-                      value={pvExtra.string_c_power}
-                      unit="W"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                  </td>
-                  <td>
-                    <span className="text--small text--subtitle-upper">
-                      String D&nbsp;
-                    </span>
-                    <NumericValue
-                      value={pvExtra.string_d_volts}
-                      unit="V"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                    <NumericValue
-                      value={pvExtra.string_d_power}
-                      unit="W"
-                      defaultValue={null}
-                      precision={1}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </MetricValues>
+          {extraVisible && (
+            <MetricValues inflate>
+              <div className="text--smaller">
+                <table cellPadding="0" cellSpacing="5">
+                  <tr>
+                    <td>
+                      <span className="text--small text--subtitle-upper">String A&nbsp;</span>
+                      <NumericValue value={string_a_volts} unit="V" defaultValue={null} precision={1} />
+                      <NumericValue value={string_a_power} unit="W" defaultValue={null} precision={1} />
+                    </td>
+                    <td>
+                      <span className="text--small text--subtitle-upper">String B&nbsp;</span>
+                      <NumericValue value={string_b_volts} unit="V" defaultValue={null} precision={1} />
+                      <NumericValue value={string_b_power} unit="W" defaultValue={null} precision={1} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span className="text--small text--subtitle-upper">String C&nbsp;</span>
+                      <NumericValue value={string_c_volts} unit="V" defaultValue={null} precision={1} />
+                      <NumericValue value={string_c_power} unit="W" defaultValue={null} precision={1} />
+                    </td>
+                    <td>
+                      <span className="text--small text--subtitle-upper">String D&nbsp;</span>
+                      <NumericValue value={string_d_volts} unit="V" defaultValue={null} precision={1} />
+                      <NumericValue value={string_d_power} unit="W" defaultValue={null} precision={1} />
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </MetricValues>
+          )}
         </ListViewWithTotals>
       </ColumnContainer>
-    );
+    )
   } else {
-    return null;
+    return null
   }
-});
+})
 
 function usePvDetail() {
   const getTopics = function () {
@@ -144,13 +109,13 @@ function usePvDetail() {
       //
       c2_daily_yield: "N/48e7da878d35/solarcharger/279/History/Daily/0/Yield",
       c1_daily_yield: "N/48e7da878d35/solarcharger/280/History/Daily/0/Yield",
-    };
-  };
+    }
+  }
   const topics = useMemo(function () {
-    return getTopics();
-  }, []);
-  useTopicSubscriptions(topics);
-  return useTopicsState(topics);
+    return getTopics()
+  }, [])
+  useTopicSubscriptions(topics)
+  return useTopicsState(topics)
 }
 
-export default Solar;
+export default Solar
